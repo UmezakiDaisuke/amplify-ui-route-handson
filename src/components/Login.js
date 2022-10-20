30a0a055cbe2f9b09fc9ee4d2ec29d80aaab82a7
UmezakiDaisuke <60737352+UmezakiDaisuke@users.noreply.github.com>
2022-10-20 19:14:28 +0900

    Amplify add auth & Amplify push

--- src/components/Login.js
+++ src/components/Login.js
@@ -6,6 +6,13 @@ import '@aws-amplify/ui-react/styles.css';
 
 import { useNavigate, useLocation } from 'react-router';
 
+
+import { I18n } from 'aws-amplify';
+import { translations } from '@aws-amplify/ui';
+I18n.putVocabularies(translations);
+// I18n.setLanguage('fr');
+
+
 export function Login() {
 
   const { route } = useAuthenticator((context) => [context.route]);
